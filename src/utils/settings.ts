294ef2bf671ab3a8576493e281064b5c7c9f1b66
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-11 02:18:17 +0100

    Update settings.ts
    back to old app id

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,6 +1,6 @@
 import { ClientConfig, createClient, createMicrophoneAndCameraTracks } from 'agora-rtc-react';
 
-export const appId = '8288b06f9d464f718a7b6264a92d6207';
+export const appId = 'fe44932382c4452a8e692e262df2ea63';
 
 export const config: ClientConfig = {
   mode: 'rtc',
