a74b9c9e5ec7af937fb744afc812ad3d9076a3a2
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-10 17:06:53 -0800

    queue match succesfully makes you join video chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React, { useState } from 'react';
 import Home from './pages/Home';
 import VideoCallPage from './pages/VideoCallPage';
+import { RoomInfo } from './types';
 
 function App() {
+  const [uid, setUid] = useState<undefined | number>(undefined);
+  const [roomInfo, setRoomInfo] = useState<undefined | RoomInfo>(undefined);
   const [inCall, setInCall] = useState(false);
 
-  return !(inCall) ? (
+  return inCall && roomInfo && uid ? (
     <VideoCallPage
+      uid={uid}
+      roomInfo={roomInfo}
       setInCall={setInCall}
     />
   ) : (
-    <Home />
+    <Home
+      uid={uid}
+      setUid={setUid}
+      setRoomInfo={setRoomInfo}
+      setInCall={setInCall}
+    />
   );
 }
 
--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,8 +1,6 @@
 import { ClientConfig, createClient, createMicrophoneAndCameraTracks } from 'agora-rtc-react';
 
-export const appId = 'fe44932382c4452a8e692e262df2ea63';
-export const token = '007eJxTYCjoSwp9IFCy4d4ajTf2plN/BHf881pfZzz/bW36llcPTx5UYEhLNTGxNDYytjBKNjExNUq0SDWzNEo1MjNKSTNKTTQzPpeXm9wQyMjAu+okEyMDBIL4zAwVlVUMDADm9yGy';
-export const channelName = 'xyz';
+export const appId = '8288b06f9d464f718a7b6264a92d6207';
 
 export const config: ClientConfig = {
   mode: 'rtc',
