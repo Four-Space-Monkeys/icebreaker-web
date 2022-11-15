bb1572e1a96ec39aa890d04d7652cd8f9d06f9ad
Cornelius Renken <cornelius.renken@gmail.com>
2022-11-15 11:36:17 -0800

    added queue page and updated functions to allow for queueing targeting selected interests

---
+++ src/features/queue/components/InterestList.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Interest } from '../../../types';
+import InterestListItem from './InterestListItem';
+
+function InterestList({
+  inQueue,
+  interests,
+  selectedInterestIds,
+  setSelectedInterestIds,
+}: {
+  inQueue: boolean;
+  interests: Interest[];
+  selectedInterestIds: number[];
+  setSelectedInterestIds: React.Dispatch<React.SetStateAction<number[]>>;
+}) {
+  function addToSelectedInterests(interestId: number) {
+    const copy = selectedInterestIds.slice();
+    copy.push(interestId);
+    setSelectedInterestIds(copy);
+  }
+
+  function removeFromSelectedInterests(interestId: number) {
+    const copy = selectedInterestIds.filter((id) => id !== interestId);
+    setSelectedInterestIds(copy);
+  }
+
+  return (
+    <div>
+      {interests.map(({ id, name }) => {
+        const selected = selectedInterestIds.includes(id);
+
+        let onClick: React.MouseEventHandler<HTMLDivElement> | undefined;
+
+        if (!inQueue) {
+          onClick = selected
+            ? () => removeFromSelectedInterests(id)
+            : () => addToSelectedInterests(id);
+        }
+
+        return (
+          <InterestListItem
+            key={id}
+            name={name}
+            selected={selected}
+            onClick={onClick}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default InterestList;
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,18 @@
 export interface RoomInfo {
   token: string;
   channel: string;
+  interest: string;
+  matchFirstName: string;
+}
+
+export interface Interest {
+  id: number;
+  name: string;
 }
 
 export interface User {
   id: number;
   firstName: string;
   lastName: string;
-  interests: { id: number, name: string }[]
+  interests: Interest[]
 }
