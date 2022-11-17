f8b1fee2914e21b09098c627aa1e6eb5008188c9
Peter McBride <mrpetermcbride@gmail.com>
2022-11-16 18:46:14 -0700

    Added registerForm for user's initial sign in
    - Check on App.tsx if user interests exist, if not route to registerForm
    - useForm build out for name and interest inputs
    - axios connection to back end

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import MenuBar from './layout/menubar/MenuBar';
 import VideoCallPage from './pages/VideoCallPage';
 import { RoomInfo, User } from './types';
 import QueuePage from './pages/QueuePage';
+import RegisterForm from './pages/RegisterForm';
 
 function App() {
   const [user, setUser] = useState<undefined | User>(undefined);
   const { session } = useStytchSession();
   const [roomInfo, setRoomInfo] = useState<undefined | RoomInfo>(undefined);
   const [inCall, setInCall] = useState(false);
+  const [registerCheck, setRegisterCheck] = useState(false);
 
   useEffect(() => {
     if (user === undefined) {
@@ -21,36 +23,62 @@ function App() {
         throw new Error('stytch id not found');
       }
 
-      axios.get(`http://localhost:8080/users/${stytchId}`)
+      axios
+        .get(`http://localhost:8080/users/${stytchId}`)
         .then(({ data }) => setUser(data))
-        .catch(() => { throw new Error('couldn\'t find user'); });
+        .catch((err) => {
+          if (err.response.status === 404) {
+            setRegisterCheck(true);
+          }
+          // throw new Error("couldn't find user");
+        });
     }
   }, [session?.user_id, user]);
 
-  return (
-    user === undefined ? (
-      // idk if we should even have this, the user fetch happens so quickly that its just a flash
-      // i'll leave it for now
+  const userAuth = user === undefined ? (
+    <div>
       <BarLoader />
-    ) : (
-      <div id="App" style={{ display: 'flex' }}>
-        <MenuBar uid={user.id} />
-        <div id="main-screen" style={{ flex: 1 }}>
-          {inCall && roomInfo ? (
-            <VideoCallPage uid={user.id} roomInfo={roomInfo} setInCall={setInCall} />
-          ) : (
-            <QueuePage
-              uid={user.id}
-              firstName={user.firstName}
-              interests={user.interests}
-              setRoomInfo={setRoomInfo}
-              setInCall={setInCall}
-            />
-          )}
-        </div>
+    </div>
+  ) : (
+    <div id="App" style={{ display: 'flex' }}>
+      <MenuBar uid={user.id} />
+      <div id="main-screen" style={{ flex: 1 }}>
+        {inCall && roomInfo ? (
+          <VideoCallPage
+            uid={user.id}
+            roomInfo={roomInfo}
+            setInCall={setInCall}
+          />
+        ) : (
+          <QueuePage
+            uid={user.id}
+            firstName={user.firstName}
+            interests={user.interests}
+            setRoomInfo={setRoomInfo}
+            setInCall={setInCall}
+          />
+        )}
       </div>
-    )
+    </div>
+  );
+
+  console.log('register', registerCheck);
+
+  return registerCheck ? (
+    <div>
+      <RegisterForm />
+    </div>
+  ) : (
+    userAuth
   );
 }
 
 export default App;
+
+// return user === undefined ? (
+//   // idk if we should even have this, the user fetch happens so quickly that its just a flash
+//   // i'll leave it for now
+//   <BarLoader />
+// ) : (
+//   registerAuth
+// );
--- src/pages/RegisterForm.tsx
+++ src/pages/RegisterForm.tsx
@@ -1,40 +1,118 @@
-import React, { useState } from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 import { useForm } from 'react-hook-form';
-import { useStytch } from '@stytch/react';
-import { TextField, Typography } from '@mui/material';
-import { LoadingButton } from '@mui/lab';
+import { ErrorMessage } from '@hookform/error-message';
+import { useStytchSession, useStytch } from '@stytch/react';
 // images / Styles
 import { Fade } from 'react-awesome-reveal';
-import Logo from '../../../assets/logos/IBLogo.png';
+// import Logo from '../../../assets/logos/IBLogo.png';
 
 type FormValues = {
   stytchId: string;
   firstName: string;
   lastName: string;
-  interestIds: number[];
+  interestIds: number;
 };
 
 export default function RegisterForm() {
-  const [submitting, setSubmitting] = useState(false);
-  const stytchClient = useStytch();
+  const { session } = useStytchSession();
+  const client = useStytch();
+  const [interestOptions, setInterestOptions] = useState([]);
+  if (!session) {
+    throw new Error('stytch id not found');
+  }
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    defaultValues: {
+      stytchId: session.user_id,
+    },
+  });
 
-  async function processSubmit(data: FormValues) {}
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/interests')
+      .then(({ data }) => setInterestOptions(data))
+      .catch(() => {
+        throw new Error('error fetching interest list');
+      });
+  }, [session?.user_id]);
+
+  async function processSubmit(data: FormValues) {
+    console.log('here is the data', data);
+    try {
+      axios.post('http://localhost:8080/users', {
+        data,
+      });
+      alert('You are now registered');
+    } catch (err: any) {
+      console.log('There was an error', err);
+    }
+  }
+
+  const handleLogout = useCallback(async () => {
+    await client.session.revoke();
+    alert('Logged Out!');
+  }, [client]);
 
   return (
-    <Fade>
-      <div className='rootContainer'>
-        <form className='form' onSubmit={handleSubmit(processSubmit)}>
-          <div className='registerContainer'>
-            <img></img>
-
+    <Fade direction="down">
+      <div className="rootContainer">
+        <form className="form" onSubmit={handleSubmit(processSubmit)}>
+          <div className="registerContainer">
+            <p className="header">
+              Fill out your Profile and choose atleast one interests
+            </p>
+            <input
+              className="inputBox"
+              placeholder="First Name"
+              {...register('firstName', {
+                required: true,
+              })}
+            />
+            <input
+              className="inputBox"
+              placeholder="Last Name"
+              {...register('lastName', {
+                required: true,
+              })}
+            />
+            {interestOptions.map((interest: { id: number; name: 'string' }) => (
+              <label htmlFor="domId" key={interest.id}>
+                {interest.name}
+                <input
+                  {...register('interestIds', {
+                    required: 'Please choose atleast one interest',
+                    valueAsNumber: true,
+                    setValueAs: (value) => parseInt(value, 10),
+                  })}
+                  type="checkbox"
+                  value={interest.id}
+                />
+              </label>
+            ))}
+            <ErrorMessage
+              errors={errors}
+              name="interestIds"
+              render={({ message }) => <p>{message}</p>}
+            />
+            <input type="submit" style={{ display: 'none' }} />
+            <button
+              type="button"
+              className="button"
+              onClick={handleSubmit(processSubmit)}
+            >
+              Save
+            </button>
           </div>
+          <button type="button" onClick={handleLogout}>
+            Logout
+          </button>
         </form>
       </div>
     </Fade>
-  )
-};
+  );
+}
